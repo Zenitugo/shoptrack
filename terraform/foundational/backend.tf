5c36013694010b6f366eeb16541f73ba87391146
zenitugo <ugochiukaegbu19@gmail.com>
2026-09-04 00:36:46 +0100

    changed s3 name

--- terraform/foundational/backend.tf
+++ terraform/foundational/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "s3" { 
-        bucket = "ugochi-project1-buck2"
+        bucket = "ugochi-shoptrack-buck1"
         key     = "shoptrack/terraform.tfstate"
         dynamodb_table = "ugochi-shoptrack-lock"
-        region = "eu-north-1"
+        region = "eu-central-1"
         encrypt = true
         use_lockfile = false
     }
