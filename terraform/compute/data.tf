d1a6d5248f781f75eb1df51cc0f647b3ef1409a0
zenitugo <ugochiukaegbu19@gmail.com>
2026-09-04 00:38:08 +0100

    changed app name

--- terraform/compute/data.tf
+++ terraform/compute/data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "foundation" {
   backend = "s3"
   config = {
-    bucket = "ugochi-project1-buck"
+    bucket = "ugochi-shoptrack-buck"
     key    = "fitness-app/terraform.tfstate"
     region = "eu-central-1"
   }
