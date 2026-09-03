e4c3442a2477e0f3997683c6ab88b97d54444d56
zenitugo <ugochiukaegbu19@gmail.com>
2026-09-04 00:38:33 +0100

    changed key name

--- terraform/compute/backend.tf
+++ terraform/compute/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" { 
         bucket = "ugochi-shoptrack-buck"
-        key     = "fitness-app/compute.tfstate"
+        key     = "shoptrack/compute.tfstate"
         dynamodb_table = "ugochi-shoptrack-lock"
         region = "eu-central-1"
         encrypt = true
